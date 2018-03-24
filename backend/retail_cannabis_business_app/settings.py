f536f52c393f1a1fd312a5fec8329cbab4efef13
Reynald Pader <rey.pader@paymaya.com>
2018-03-24 15:49:37 +0800

    Prepare prod deployment

--- backend/retail_cannabis_business_app/settings.py
+++ backend/retail_cannabis_business_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['retail-cannabis-business-test.appspot.com', '127.0.0.1']
+ALLOWED_HOSTS = ['retail-cannabis-business.appspot.com', '127.0.0.1']
 
 # Application definition
 
